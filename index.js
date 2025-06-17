05e9f94e882b6012a1f2dcf6ecc9024e212971f0
Xisachii <csgogo997@gmail.com>
2025-06-17 12:29:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-async function loadUsers() {
+=async function loadUsers() {
   try {
-    const response = await fetch("http://192.168.100.91:8000/users");
+    const response = await fetch("https://mylink.loca.lt/users"); // <-- LocalTunnel linkni shu yerga yozing
     const users = await response.json();
 
     const userList = document.getElementById("user-list");
     userList.innerHTML = "";
 
-    for (let i = 0; i < users.length; i += 5) {
-      const row = document.createElement("div");
-      row.className = "user-row";
-
-      users.slice(i, i + 5).forEach(user => {
-        const div = document.createElement("div");
-        div.className = "user";
-        div.innerText = user.name;
-        row.appendChild(div);
-      });
-
-      userList.appendChild(row);
-    }
+    users.forEach((user, index) => {
+      const div = document.createElement("div");
+      div.className = "user";
+      div.innerText = user.name;
+      userList.appendChild(div);
+    });
   } catch (error) {
-    console.error("Xatolik:", error);
-    document.getElementById("user-list").innerText = "Ma'lumotlarni olishda xatolik yuz berdi.";
+    alert("Ma'lumotlarni olishda xatolik yuz berdi.");
+    console.error(error);
   }
 }
 
